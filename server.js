2952b11a3996d43ad7f90bc34bde4161805b05ba
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2013-02-13 22:04:12 -0200

    initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+﻿var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain; charset="UTF-8"'});
+  res.end('à recherche du temps perdu...\n');
+}).listen(1337, '127.0.0.1');
+console.log('Proust running at http://127.0.0.1:1337/');

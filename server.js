ff91a1505c374499a5ee8915f9fb0e6078cb621a
robertaarcoverde <roberta.arcoverde@gmail.com>
2013-02-14 23:29:16 -0200

    broadcasting and logging in

--- server.js
+++ server.js
@@ -2,9 +2,29 @@
 var clients = [];
 
 net.createServer(function (client) {
-  clients.push(client);  
+  clients.push(client);
+  client.write("à la recherche du temps perdu... oh, mon ami, who art thou?");
   
-  client.write("à la recherche du temps perdu...");	
+  client.on('data', function(data){
+	if(!client.name) {
+		client.name = data;
+		console.log(client.name + " has joined proust. sounds fun.");
+	} else {
+		client.broadcast(data);
+	}
+  });  
+  
+  client.on('end', function(){
+	var i = clients.indexOf(client);
+	clients.splice(i,1);
+  });
+  
+  client.broadcast = function(data) {
+	for(var i = 0; i < clients.length; i++) {
+		if(clients[i] == this) continue;
+		clients[i].write(this.name + ">" + data);
+	}
+  };
   
 }).listen(1337);
 console.log('Proust running (on TCP) at port 1337');
